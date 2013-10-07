467e2965eb3e2795495103d1fd80fa96b0035267
kuzaleks <kuzAleksAleks@gmail.com>
2013-10-07 10:14:42 +0300

    The test data centralisation and transform have been implemented

--- kernelmethods.py
+++ kernelmethods.py
@@ -6,15 +6,18 @@ import mlpy
 class KernelMethod:
     def __init__(self, kernel_func):
         self.kernel_func = kernel_func
-    def estim_kbasis(self, trainData):
+    def estim_kbasis(self, trData):
         pass
     def transform(self, data, trDim):
         pass
 
 class kPCA(KernelMethod):
-    def estim_kbasis(self, trainData):
-        Kx = np.mat([[kernel_func(xi, xj) for xj in data] for xi in data])
+    def estim_kbasis(self, trData):
+        Kx = np.mat([[self.kernel_func(xi, xj) for xj in trData] for xi in trData])
         Kx = np.mat(mlpy.kernel_center(Kx, Kx))
         vals, vecs = np.linalg.eig(Kx)
-
-
+        norm_mat = np.mat(np.diag([1.0/np.sqrt(vals[i]) for i in range(len(vals))]))
+        vecs = vecs * norm_mat
+        print 'mult:', vals[0] * vecs[:, 0].T * vecs[:, 0]
+    def transform(self, data):
+        pass
--- mainkernapp.py
+++ mainkernapp.py
@@ -26,21 +26,19 @@ def polynomial_closure(ext):
     return polynomial
 
 
-def draw_data(data, clabs, kernel_func):
+def draw_data(data, clabs, kernel_func, testData):
     fig = plt.figure(1) # plot
 
     print data.shape
     ax1 = plt.subplot(121)
     plot1 = plt.scatter(data[:, 0], data[:, 1], c=clabs)
+    plot1_5 = plt.scatter(testData[:, 0], testData[:, 1])
 
     Kx = np.mat([[kernel_func(xi, xj) for xj in data] for xi in data])
+    KxCopy = Kx
     Kx = np.mat(mlpy.kernel_center(Kx, Kx))
+    print "KxCopy == Kx", np.allclose(KxCopy, Kx)
     vals, vecs = np.linalg.eig(Kx)
-
-    gK = mlpy.kernel_gaussian(data, data, sigma=2)
-    print "gK is eqaul to Kx", np.allclose(gK, Kx)
-    gaussian_pca = mlpy.KPCA()
-    gaussian_pca.learn(gK)
 
     print "mult before normalization", vals[0]*vecs[:, 0].T*vecs[:, 0]
 
@@ -48,32 +46,46 @@ def draw_data(data, clabs, kernel_func):
     vecs = vecs * norm_mat
     print 'mult:', vals[0] * vecs[:, 0].T * vecs[:, 0]
 
-    stream = open('norm_vecs.yaml', 'w')
-    yaml.dump(np.real(vecs), stream, default_flow_style=False)
-    stream.close()
+    gK = mlpy.kernel_gaussian(data, data, sigma=2)
+    gaussian_pca = mlpy.KPCA(mlpy.KernelGaussian(2.0))
+    gaussian_pca.learn(data)
 
-    mlpy_vecs = np.mat(gaussian_pca.coeff())
-    print 'mlpy_mult:', gaussian_pca.evals()[0] * mlpy_vecs[:, 0].T * mlpy_vecs[:, 0]
+#    vals = np.mat(gaussian_pca.evals())
+#    vecs = np.mat(gaussian_pca.coeff())
+#    Kx = gK
+#    Kx = np.mat(mlpy.kernel_center(Kx, Kx))
+    
+
     data_k_trans = np.real(Kx.T * vecs[:, :2])
     data_k_trans = np.array(data_k_trans)
     ax2 = plt.subplot(122)
     plot2 = plt.scatter(data_k_trans[:, 0], data_k_trans[:, 1], c=clabs)
 
+    Kt = np.mat([[kernel_func(xi, xj) for xj in data] for xi in testData])
+    Kt = np.mat(mlpy.kernel_center(Kt, KxCopy))
+    kTransTestData = np.real(Kt * vecs[:, :2])
+    kTransTestData = np.array(kTransTestData)
+    
+    plot2_5 = plt.scatter(kTransTestData[:, 0], kTransTestData[:, 1])
+
     plt.show()
 
 
-def draw_mlpy_example(data, clabs):
+def draw_mlpy_example(data, clabs, testData):
     gK = mlpy.kernel_gaussian(data, data, sigma=2) # gaussian kernel matrix
-    gaussian_pca = mlpy.KPCA()
-    gaussian_pca.learn(gK)
-    gz = gaussian_pca.transform(gK, k=2)
+    gaussian_pca = mlpy.KPCA(mlpy.KernelGaussian(2.0))
+    gaussian_pca.learn(data)
+    gz = gaussian_pca.transform(data, k=2)
 
     fig = plt.figure(1)
     ax1 = plt.subplot(121)
     plot1 = plt.scatter(data[:, 0], data[:, 1], c=clabs)
+    plot1_5 = plt.scatter(testData[:, 0], testData[:, 1])
     title1 = ax1.set_title('Original data')
+    trTestData = gaussian_pca.transform(testData, k=2)
     ax2 = plt.subplot(122)
     plot2 = plt.scatter(gz[:, 0], gz[:, 1], c=clabs)
+    plot2_5 = plt.scatter(trTestData[:, 0], trTestData[:, 1])
     title2 = ax2.set_title('Gaussian kernel')
     plt.show()
 
@@ -104,10 +116,17 @@ def main():
         x[100:150, 1] = r * np.sin(theta)
         y[100:150] = 2
 
+        tSamplesTotal = 50
+        testData = np.zeros((tSamplesTotal, 2))
+        theta = np.random.normal(0, np.pi, tSamplesTotal)
+        r = np.random.normal(3, 0.1, tSamplesTotal)
+
+        testData[:, 0] = r * np.sin(theta)
+        testData[:, 1] = r * np.cos(theta)
 #        kernel_func = partial(mlpy.kernel_gaussian, sigma=2.0)
         kernel_func = rbf_closure(2.0)
-        draw_data(x, y, kernel_func)
-        draw_mlpy_example(x, y)
+        draw_data(x, y, kernel_func, testData)
+        draw_mlpy_example(x, y, testData)
 
 
 if __name__ == "__main__":
